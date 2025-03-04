458d7f8d514e1ed2da762e03f5bfc6f391605684
yheechan <heechy06@naver.com>
2025-03-04 18:25:06 +0900

    Charlie solved BJ silver 9012.cpp

---
+++ charlie/baekjoon/silver/9012.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  int N;
+  cin >> N;
+
+  stack<char> lp;
+  stack<char> rp;
+
+  string stream;
+  int status;
+  for (int i=0; i<N; i++) {
+    stack<char> S;
+    status = 0;
+    cin >> stream;
+
+    for (int j=0; j<stream.size(); j++) {
+      // at left-paranthesis (LP)
+      if (stream[j] == '(') S.push(stream[j]);
+      // at right-paranethsis, check stack
+      else {
+        // if stack is not empty, pop one LP
+        if (!S.empty() && S.top() == '(') {
+          S.pop();
+        }
+        // if stack is empty, than this is not an VPS
+        else {
+          status = 1;
+          break;
+        }
+      }
+    }
+    if (!S.empty()) {cout << "NO\n"; continue;}
+    if (status == 1) cout << "NO\n";
+    else cout << "YES\n";
+  }
+
+  return 0;
+}
