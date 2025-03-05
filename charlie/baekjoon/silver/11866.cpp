a62a881671ae164848a0e815202adbc1f5f48f18
yheechan <heechy06@naver.com>
2025-03-05 21:21:44 +0900

    Charlie working on BJ silver 11866.cpp

---
+++ charlie/baekjoon/silver/11866.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N, K;
+  cin >> N >> K;
+
+  vector<int> JOSEPHUS(N);
+  for (int i=0; i<N; i++) {
+
+  }
+
+  return 0;
+}
