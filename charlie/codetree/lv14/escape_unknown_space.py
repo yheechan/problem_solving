909024ee8e638fc1e11ab4fdabdaaf1a5c67271f
yheechan <heechy06@naver.com>
2025-03-21 21:20:20 +0900

    Init samsung escape_unkown_space lv14 bfs

---
+++ charlie/codetree/lv14/escape_unknown_space.py
@@ -0,0 +1,17 @@
+
+
+class TimeMachine:
+  def __init__(self):
+    pass
+
+  def overview(self):
+    pass
+
+  def timewall_view(self):
+    pass
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
