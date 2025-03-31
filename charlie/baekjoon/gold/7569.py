97904dc43c73d6156ec3b8ff779e47fcbee150ae
yheechan <heechy06@naver.com>
2025-03-31 16:33:44 +0900

    Charlie solved BJ gold 7569 BFS: 3d tomato farm

--- charlie/baekjoon/gold/7569.py
+++ charlie/baekjoon/gold/7569.py
@@ -1,32 +1,32 @@
 from collections import deque
 
-DIR = [[0, 1], [1, 0], [0, -1], [-1, 0]] # N, E, S, W
+DIR = [[0, 1, 0], [1, 0, 0], [0, -1, 0], [-1, 0, 0], [0, 0, 1], [0, 0, -1]] # N, E, S, W, U, D
 
-def within_range(x, y, x_lim, y_lim):
-  if (x>-1 and x_lim>x) and (y>-1 and y_lim>y):
+def within_range(x, y, z, x_lim, y_lim, z_lim):
+  if (x>-1 and x_lim>x) and (y>-1 and y_lim>y) and (z>-1 and z_lim>z):
     return True
   return False
 
-def bfs(M, N, farm, traverse, empty):
+def bfs(M, N, H, farm_stack, traverse, empty):
   result_days = -1
   ryped = len(traverse)
 
   while traverse:
-    x, y, curr_day = traverse.popleft()
+    x, y, z, curr_day = traverse.popleft()
     if curr_day > result_days:
       result_days = curr_day
 
-    for c in range(4):
-      check_x, check_y = x+DIR[c][0], y+DIR[c][1]
+    next_day = curr_day + 1
+    for c in range(6):
+      check_x, check_y, check_z = x+DIR[c][0], y+DIR[c][1], z+DIR[c][2]
 
       # RYPE THE TOMATO
-      if within_range(check_x, check_y, M, N) and farm[check_y][check_x] == 0:
-        next_day = curr_day + 1
-        farm[check_y][check_x] = 1
+      if within_range(check_x, check_y, check_z, M, N, H) and farm_stack[check_z][check_y][check_x] == 0:
+        farm_stack[check_z][check_y][check_x] = 1
         ryped += 1
-        traverse.append((check_x, check_y, next_day))
+        traverse.append((check_x, check_y, check_z, next_day))
   
-  if ryped != (N*M)-empty:
+  if ryped != (N*M*H)-empty:
     return -1
 
   return result_days
@@ -34,26 +34,27 @@ def bfs(M, N, farm, traverse, empty):
 def main():
   # M: horizontal (가로)
   # N: vertical (세로)
-  M, N = map(int, input().split())
+  # H: stacks
+  M, N, H = map(int, input().split())
 
   traverse = deque()
   empty = 0
-  farm = []
+  farm_stack = []
 
-  for j in range(N):
-    row = list(map(int, input().split()))
-    farm.append(row)
-    for i in range(M):
-      if row[i] == 1:
-        traverse.append((i, j, 0))
-      if row[i] == -1:
-        empty += 1
+  for k in range(H):
+    farm = []
+    for j in range(N):
+      row = list(map(int, input().split()))
+      farm.append(row)
+      for i in range(M):
+        if row[i] == 1:
+          traverse.append((i, j, k, 0))
+        if row[i] == -1:
+          empty += 1
+    farm_stack.append(farm)
 
-
-  res = bfs(M, N, farm, traverse, empty)
+  res = bfs(M, N, H, farm_stack, traverse, empty)
   print(res)
-
-    
 
 if __name__ == "__main__":
   main()
