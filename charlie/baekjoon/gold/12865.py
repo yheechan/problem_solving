03006b412a64c980778699589d2858f94d2269e3
yheechan <heechy06@naver.com>
2025-04-01 09:15:55 +0900

    Charlie working on BJ gold 12865 DP: knapsack

--- charlie/baekjoon/gold/12865.py
+++ charlie/baekjoon/gold/12865.py
@@ -1,26 +1,41 @@
 
+
+
+
 def dp(items, N, K):
-  max_value = -1
+    n = N
+    dp = [[0]*(K+1) for _ in range(N+1)]
 
-    
+    # for each item
+    for i in range(N+1):
+        # for each weight
+        for j in range(K+1):
+
+            if i==0 and j==0:
+                dp[i][j] = 0
+            else:
+                pick = 0
+
+                if items[i-1][
 
 
-
-  return -1
-
-
+    return res
 
 
 def main():
-  N, K = map(int, input().split())
-  items = []
-  for _ in range(N):
-    W, V = map(int, input().split())
-    items.append((W, V))
+    # N: number of items
+    # K: weight limit
+    N, K = map(int, input().split())
+    items = []
+    for _ in range(N):
+        W, V = map(int, input().split())
+        items.append((W, V))
 
-  res = dp(items, N, K)
-  print(res)
+    res = dp(items, N, K)
+    print(res)
 
 if __name__ == "__main__":
-  main()
+
+    main()
+
 
