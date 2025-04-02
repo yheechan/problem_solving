62dce6168a7e788c66d2fb99eebe428ce26044a9
yheechan <heechy06@naver.com>
2025-04-02 13:21:38 +0900

    Charlie working on BJ gold 1967 DFS problem

---
+++ charlie/baekjoon/gold/1967.py
@@ -0,0 +1,14 @@
+
+
+
+def main():
+    # n: number of nodes (1<=n<=10,000)
+    n = int(input())
+
+    for _ in range(n):
+        u, v, w = map(int, input().split())
+
+
+if __name__ == "__main__":
+    main()
+
