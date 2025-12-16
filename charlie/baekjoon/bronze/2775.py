b9b8a519c08667dff3e11d3ead41ece2539fba3b
yheechan <heechy06@naver.com>
2025-12-16 22:18:10 +0900

    Charlie solved 2775 bronze baekjoon

--- charlie/baekjoon/bronze/2775.py
+++ charlie/baekjoon/bronze/2775.py
@@ -1,29 +1,26 @@
 T = int(input())
 
-room_data = [[0] * 15 for _ in range(15)]
-
-
 test = []
+max_n = -1
 for i in range(T):
-    k = int(input())
-    n = int(input())
-    test.append((k, n))
+  k = int(input())
+  n = int(input())
+  if n > max_n:
+      max_n = n
+  test.append((k, n))
+
+room_data = [[0] * 15 for _ in range(15)]
+for i in range(max_n):
+    room_data[0][i] = i+1
+
+
+for floor in range(1, 15):
+  for room in range(max_n):
+    for r in range(room+1):
+      room_data[floor][room] += room_data[floor-1][r]
+
 
 for k, n in test:
-    # fill 0th floor
-    if k == 1:
-        for i in range(n):
-            room_data[k-1][i] = i+1
+  print(room_data[k][n-1])
 
-    # fill current floor
-    for room in range(n):
-        for below_room in range(room+1):
-            if room_data[k-1][below_room] == 0:
-                # fill this room first
-                for r in range(below_room+1):
-                    room_data[k-1][below_room] += room_data[k-2][r]
-                    
-            room_data[k][room] += room_data[k-1][below_room]
-
-    print(room_data[k][n-1])
 
